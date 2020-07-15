8afea6b9f5684bb19f8ed6b62e803dc18411704d
Webaxones <contact@webaxones.com>
2020-07-15 13:14:35 +0200

    Initial commit

---
+++ wax-disable-woocommerce-admin-and-analytics.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: WAX disable woocommerce admin and woocommerce analytics
+ * Plugin URI: https://www.webaxones.com
+ * Description: Disable WooCommerce Admin and WooCommerce Analytics
+ * Author: Webaxones
+ * Author URI: https://www.webaxones.com
+ * Version: 1.0
+ */
+
+// don't load directly.
+if ( ! defined( 'ABSPATH' ) ) :
+	die( '-1' );
+endif;
+
+/**
+ * Disable WooCommerce Admin and WooCommerce Analytics
+ */
+
+add_filter( 'woocommerce_admin_disabled', '__return_true' );
